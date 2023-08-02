dde65ff6b141ef861987dfb610b31251955fb6c3
LAQNT <laura.quintero.a@hotmail.com>
2023-08-02 13:02:36 +0200

    updated BASE_URL

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://localhost:3001/api/v1";
-export const BASE_URL = "https://venturo-server.onrender.com";
+export const BASE_URL = "https://venturo-server.onrender.com/api/v1";
